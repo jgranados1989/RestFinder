17b22cc25167ebf8a3622719db57934994308e81
Esteban Leandro <elndro85@gmail.com>
2014-04-28 19:58:06 -0600

    cambios en restaurante.py

--- RESPONSIVE_DESIGN/responsive/restaurantes.py
+++ RESPONSIVE_DESIGN/responsive/restaurantes.py
@@ -9,18 +9,26 @@ Lista de restaurantes
 '''
 def imprimirRest():
 	p.consult("rest.pl")
+	resultados=[]
 	for i in p.query("restaurante(A,_,_,_,_)"): #hago una consulta e imprimo todos los que cumplen la consulta
-		print i["A"]
+		#print str(cont)+i["A"] Impresion de los restaurantes uno por uno
+		resultados.append(str(i["A"]))
+	print resultados
 	print "========== Fin de restaurantes =========="
+	return resultados
 
 '''
 Lista de platillos
 '''
 def imprimirPlato():
 	p.consult("plati.pl")
+	resultados=[]
 	for plato in p.query("platillo(_,A,_,_,_)"):
-		print plato["A"]
+		resultados.append(str(plato["A"]))
+		#print plato["A"]
+	print resultados
 	print "========== Fin de platillos =========="
+	return resultados
 
 '''
 Lista de restaurantes filtrados por tipo de comida
@@ -28,10 +36,14 @@ Entradas: tipo de comida a buscar (comidaRapida,comidaGourmet,pizzeria,etc)
 '''
 def restaurantesXtipo(tipocomida):
 	p.consult("rest.pl")
+	resultados=[]
 	print ("Restaurantes que venden "+tipocomida+":")
 	for restaurante in p.query("restaurantesXtipo(A,"+tipocomida+")"):
-		print restaurante["A"]
+		resultados.append(str(restaurante["A"]))
+		#print restaurante["A"]
+	print resultados
 	print "========== Fin de consulta =========="
+	return resultados
 
 '''
 Busqueda de restaurantes por nombre	
@@ -39,14 +51,20 @@ Entradas: nombre del restaurante
 '''
 def buscaRestaurantesXNombre(nombre):
 	p.consult("rest.pl")
+	resultados=[]
 	print ("Informacion de restaurantes llamados "+nombre+":")
 	for restaurante in p.query("buscaRest("+nombre+",A,B,C,D)"):
-		print "Tipo comida: " +restaurante["A"]
-		print "Ubicacion: "+restaurante["B"]
-		print 'Telefono: '+str(restaurante["C"])
-		print "Jornada: "+restaurante["D"]
-		print "========== Fin de restaurante =========="
+		temporal=[]
+		temporal.append("Tipo comida: " +restaurante["A"])
+		temporal.append("Ubicacion: "+restaurante["B"])
+		temporal.append('Telefono: '+str(restaurante["C"]))
+		temporal.append("Jornada: "+restaurante["D"])
+		temporal.append("========== Fin de restaurante ==========")
+		resultados.append(temporal)
+		temporal=[]
+	print resultados
 	print "========== Fin de consulta =========="
+	return resultados
 
 '''
 Lista de restaurantes que tienen platillos de algun pais especifico
@@ -54,10 +72,14 @@ Entradas: pais a buscar
 '''
 def buscaRestaurantesXPais(pais):
 	p.consult("plati.pl")
+	resultados=[]
 	print ("Restaurantes con platillos cuyo pais de origen es "+pais+":")
 	for restaurante in p.query("buscaRestXPais(Nombre,"+pais+")"):
-		print "Nombre: " +restaurante["Nombre"]
+		#print "Nombre: " +restaurante["Nombre"]
+		resultados.append("Nombre: " +restaurante["Nombre"])
+	print resultados
 	print "========== Fin de consulta =========="
+	return resultados
 
 '''
 Lista de platillos de un restaurante especifico	
@@ -65,16 +87,23 @@ Entradas: nombre del restaurante a buscar
 '''
 def buscaPlatillosRest(restaurante):
 	p.consult("plati.pl")
+	resultados=[]
 	print ("Platillos de:"+restaurante+":")
 	for restaurante in p.query("platillosXrest("+restaurante+",Nombre,Sabor,Pais,Ingredientes)"):
-		print "Nombre: " +restaurante["Nombre"]
-		print "Sabor: " +restaurante["Sabor"]
-		print "Pais de origen: " +restaurante["Pais"]
-		print "Ingredientes: "
-		for i in list(restaurante["Ingredientes"]):
-			print i
-		print "========== Fin de platillo =========="
+		temporal=[]
+		temporal.append("Nombre: " +restaurante["Nombre"])
+		temporal.append("Sabor: " +restaurante["Sabor"])
+		temporal.append("Pais de origen: " +restaurante["Pais"])
+		temporal.append(["Ingredientes: "]+list(restaurante["Ingredientes"]))
+		#temporal.append(restaurante["Ingredientes"])
+		#for i in list(restaurante["Ingredientes"]):
+		#	print i
+		temporal.append("========== Fin de platillo ==========")
+		resultados.append(temporal)
+		temporal=[]
+	print resultados
 	print "========== Fin de consulta =========="
+	return resultados
 
 '''
 Lista de platillos de un restaurante especifico que tengan un ingrediente en particular
@@ -82,44 +111,37 @@ Entradas: nombre del restaurante y el ingrediente que se desea buscar
 '''
 def platillosXrestIngrediente(restaurante,ingrediente):
 	p.consult("plati.pl")
-	print ("Lista de platillos de "+restaurante+" que incluyen "+ingrediente+":")
+	resultados=[]
+	resultados.append("Lista de platillos de "+restaurante+" que incluyen "+ingrediente+":")
 	#platillosXrestIngrediente(X,Nombre,Sabor,Pais,Y)
 	for restaurante in p.query("platillosXrestIngrediente("+restaurante+",Nombre,Sabor,Pais,"+ingrediente+")"):
-		print restaurante["Nombre"]
+		resultados.append(restaurante["Nombre"])
+	print resultados
 	print "========== Fin de consulta =========="
+	return resultados
 
 '''
 Agrega un restaurante a la base de conocimientos
 '''
 def agregarRest(nombre,tipoComida,ubicacion,telefono,horario):
 	arch = file("rest.pl","a")
-	functor="\nrestaurante("
+	functor="restaurante("
 	functor=functor+nombre+","+tipoComida+","+ubicacion+","+telefono+","+horario+")."
 	arch.write(functor)
-	#p.assertz("'"+functor+"'") #con esto carga las varas a la base de conocimientos
+	p.assertz("'"+functor+"'") #con esto carga las varas a la base de conocimientos
 
 '''
 Agrega un platillo a la base de conocimientos
 '''
 def agregarPlatillo(rest,nombrePlat,sabor,PaisOrg,Ingredientes): #sabor puede ser picante, salado, dulce, agridulce, amargo
 	arch = file("plati.pl","a")
-	functor="\nplatillo("
+	functor="platillo("
 	functor=functor+rest+","+nombrePlat+","+sabor+","+PaisOrg+","+Ingredientes+")." #Ingredientes por ahora lo agrego como si fuera una lista de python, creo que en prolog el manejo es algo similar
 	arch.write(functor)
-	#p.assertz("'"+functor+"'") #con esto carga las varas a la base de conocimientos
+	p.assertz("'"+functor+"'") #con esto carga las varas a la base de conocimientos
 
 '''
 Funcion de ejecucion de pruebas
 '''
-def pruebas():
-	agregarRest("mC","hamburguesas","cartago","25354545","tod_el_dia")
-	#imprimirRest()
-	#restaurantesXtipo("comidaRapida")
-	#restaurantesXtipo("comidaGourmet")
-	#buscaRestaurantesXNombre("mcDonalds")
-	#buscaRestaurantesXPais("alemania")
-	#buscaPlatillosRest("bk")
-	#platillosXrestIngrediente("bk","pan")
-	#platillosXrestIngrediente("mcDonadls","carne")
 
-#pruebas()
+
